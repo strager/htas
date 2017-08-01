98a3fbfe6224ab204868fcc911b58d957ecf04d8
Matthew Glazar <strager.nds@gmail.com>
2017-07-31 22:23:01 -0700

    parallelism! kinda shitty though...

--- Strager.hs
+++ Strager.hs
@@ -22,6 +22,12 @@ import Text.Printf
 import Control.Monad.Free (Free(Free, Pure), liftF)
 import Control.Monad.Trans.State (evalState, state)
 import Data.List (nub, permutations)
+import Control.Concurrent.STM (atomically)
+import Control.Concurrent.STM.TChan (TChan, newTChan, readTChan, writeTChan)
+import Control.Concurrent.MVar (MVar, newMVar, withMVar)
+import Control.Concurrent.STM.TVar (TVar, newTVar, readTVar, readTVarIO, writeTVar)
+import Control.Exception (bracket)
+import Control.Concurrent.Async (forConcurrently)
 
 import HTas.Direct
 import HTas.Low
@@ -80,15 +86,9 @@ import Red.Save
 
 main :: IO ()
 main = do
-    hSetBuffering stdout NoBuffering
-
     baseSave <- BS.readFile "pokered_dugtrio3.sav"
 
-    gb <- create
-    loadRomFile gb "pokered.gbc"
-
-    beginState <- saveState gb
-    _paths <- runSearch gb beginState $ do
+    _paths <- runSearch newGB $ do
         (frame, _) <- segment "IGT frame" [0] $ \gb _inputRef frame -> do
             loadSaveData gb (setSaveFrames frame baseSave)
             reset gb
@@ -96,6 +96,13 @@ main = do
             return $ Just ()
         dugtrio
     return ()
+
+    where
+    newGB :: IO GB 
+    newGB = do
+        gb <- create
+        loadRomFile gb "pokered.gbc"
+        return gb
 
 data SearchDSL f where
     Log :: String -> f -> SearchDSL f
@@ -107,47 +114,79 @@ instance Functor SearchDSL where
 
 type Search = Free SearchDSL
 
-runSearch :: GB -> ByteString -> Search a -> IO [a]
-runSearch gb beginState search = case search of
-    Free (Log message continue) -> do
-        putStr message
-        runSearch gb beginState continue
-    Free (Segment name paths apply continue) -> do
-        -- printf "%s: Begin\n" name
-        goodSegments <- runSegmentPaths paths apply
-        -- printf "%s: Found %d unique successful paths (from %d input paths)\n" name (Map.size goodSegments) (length paths)
-        outs <- forM (Map.assocs goodSegments) $ \(result, (path, endState))
-            -> runSearch gb endState $ continue (path, result)
-        -- printf "%s: End\n" name
-        return $ concat outs
-    Pure x -> return [x]
+runSearch :: IO GB -> Search a -> IO [a]
+runSearch newGB search = do
+    gbs <- replicateM 8 newGB
+    gbChan <- atomically $ do
+        gbChan <- newTChan
+        mapM_ (writeTChan gbChan) gbs
+        return gbChan
+    runCountVar <- atomically $ newTVar (0 :: Int)
+    logMutex <- newMVar ()
+    -- TODO(strager): Clean up this thread.
+    forkIO $ forever $ do
+        threadDelay (3 * 1000 * 1000)
+        count <- readTVarIO runCountVar
+        log logMutex $ printf "%d runs executed\n" count
+    runSearch' gbChan runCountVar logMutex search
 
     where
-    runSegmentPaths :: (Ord s) => [a] -> (GB -> IORef Input -> a -> IO (Maybe s)) -> IO (Map s (a, ByteString))
-    runSegmentPaths paths apply = do
-        resultToPathAndStateRef <- newIORef Map.empty
-        gen <- newStdGen
-        -- HACK(strager): Make things faster.
-        let paths' = takeRandom 30 paths gen
-        forM_ paths' $ \path -> do
-            inputRef <- newIORef mempty
-            setInputGetter gb (readIORef inputRef)
-            loadState gb beginState
-            printf "."
-            maybeResult <- apply gb inputRef path
-            case maybeResult of
-                Just result -> do
-                    s <- readIORef resultToPathAndStateRef
-                    -- TODO(strager): Evaluate cost and pick
-                    -- the cheaper version.
-                    let shouldInsert = result `Map.notMember` s
-                    when shouldInsert $ do
-                        endState <- saveState gb
-                        writeIORef resultToPathAndStateRef (Map.insert result (path, endState) s)
-                    return ()
-                Nothing -> return ()
-        printf "\n"
-        readIORef resultToPathAndStateRef
+    log :: MVar () -> String -> IO ()
+    log logMutex message = withMVar logMutex $ \_ -> hPutStr stderr message
+
+    runSearch' :: TChan GB -> TVar Int -> MVar () -> Search a -> IO [a]
+    runSearch' gbChan runCountVar logMutex search
+        = runSearch'' Nothing search
+
+        where
+        runSearch'' :: Maybe ByteString -> Search a -> IO [a]
+        runSearch'' maybeBeginState search = case search of
+            Free (Log message continue) -> do
+                log logMutex message
+                runSearch'' maybeBeginState continue
+            Free (Segment name paths apply continue) -> do
+                -- log $ printf "%s: Begin\n" name
+                goodSegments <- runSegmentPaths maybeBeginState paths apply
+                -- log $ printf "%s: Found %d unique successful paths (from %d input paths)\n" name (Map.size goodSegments) (length paths)
+                outs <- forConcurrently (Map.assocs goodSegments) $ \(result, (path, endState))
+                    -> runSearch'' (Just endState) $ continue (path, result)
+                -- putStrLn stderr $ printf "%s: End\n" name
+                return $ concat outs
+            Pure x -> return [x]
+
+        runSegmentPaths :: (Ord s) => Maybe ByteString -> [a] -> (GB -> IORef Input -> a -> IO (Maybe s)) -> IO (Map s (a, ByteString))
+        runSegmentPaths maybeBeginState paths apply = do
+            resultsVar <- atomically $ newTVar Map.empty
+            gen <- newStdGen
+            -- HACK(strager): Make things faster.
+            let paths' = takeRandom 30 paths gen
+            forConcurrently paths' $ \path -> bracket
+                (atomically $ readTChan gbChan)
+                (\gb -> atomically $ writeTChan gbChan gb)
+                $ \gb -> do
+                    inputRef <- newIORef mempty
+                    setInputGetter gb (readIORef inputRef)
+                    case maybeBeginState of
+                        Just beginState -> loadState gb beginState
+                        Nothing -> return ()
+                    maybeResult <- apply gb inputRef path
+                    atomically $ do
+                        count <- readTVar runCountVar
+                        writeTVar runCountVar (count + 1)
+                    case maybeResult of
+                        Just result -> do
+                            results <- readTVarIO resultsVar
+                            if result `Map.member` results
+                                -- TODO(strager): Evaluate cost and pick the cheaper version.
+                                then return ()
+                                else do
+                                    endState <- saveState gb
+                                    atomically $ do
+                                        results <- readTVar resultsVar
+                                        -- TODO(strager): Evaluate cost and pick the cheaper version.
+                                        writeTVar resultsVar $ Map.insert result (path, endState) results
+                        Nothing -> return ()
+            readTVarIO resultsVar
 
 segment :: (Ord s) => String -> [a] -> (GB -> IORef Input -> a -> IO (Maybe s)) -> Search (a, s)
 segment name paths apply = liftF $ Segment name paths apply id
@@ -196,9 +235,11 @@ dugtrio = do
         return $ Just (last path, rngState)
 
     let segment2Paths = do
-            basePath <- nub $ permutations [i_Up, i_Up, i_Right, i_Right]
-            let basePath' = i_Up : basePath
-            let aPaths = pressAArbitrarily basePath'
+            basePath <- concat
+              [ map (i_Up :) $ nub $ permutations [i_Up, i_Up, i_Right, i_Right]
+              , map (\path -> i_Right : i_Up : path) $ nub $ permutations [i_Up, i_Up, i_Right]
+              ]
+            let aPaths = pressAArbitrarily basePath
             -- Don't press A if we would interact with the
             -- dude.
             let aPaths' = filter (\path -> not $
